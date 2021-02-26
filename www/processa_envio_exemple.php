a8e51d7be953034c2d74bac5525542e3b7588c9d
Anderson Teala <andersonteala@gmail.com>
2021-02-26 11:43:42 -0300

    Update file

--- www/processa_envio_exemple.php
+++ www/processa_envio_exemple.php
@@ -13,6 +13,7 @@ require "vendor/autoload.php";
       private $para = null;
       private $assunto = null;
       private $mensagem = null;
+      public $status = array( 'codigo_status' => null, 'descricao_status' => '' );
 
       public function __get($atributo) {
         return $this->$atributo;
@@ -42,13 +43,13 @@ require "vendor/autoload.php";
 
     if(!$mensagem->mensagemValida()) {
         echo 'Mensagem não é válida';
-        die();
+        header('Location: index.php');
     }
 
     $mail = new PHPMailer(true);
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->SMTPDebug = false;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -59,7 +60,7 @@ require "vendor/autoload.php";
 
         //Recipients
         $mail->setFrom('test@mailhog.local', 'Spaceatm Remetente');
-        $mail->addAddress('test@mailhog.local', 'Spaceatm Destinatário');     //Add a recipient
+        $mail->addAddress($mensagem->__get('para'));     //Add a recipient
         //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addAddress('ellen@example.com');               //Name is optional
         //$mail->addCC('cc@example.com');
@@ -71,12 +72,59 @@ require "vendor/autoload.php";
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Oi. Eu sou o assunto';
-        $mail->Body    = 'Oi. Eu sou o conteúdo do <strong>e-mail</strong>.';
-        $mail->AltBody = 'Oi. Eu sou o conteúdo do e-mail';
+        $mail->Subject = $mensagem->__get('assunto');
+        $mail->Body    = $mensagem->__get('mensagem');
+        $mail->AltBody = 'Necessário utilizar um cliente que suporte HTML para essa mensagem';
 
         $mail->send();
-        echo 'Message has been sent';
+        $mensagem->status['codigo_status'] = 1;
+        $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso';
     } catch (Exception $e) {
-        echo "🙁 Não foi possível enviar este e-mail, por favor tente novamente Detalhes do erro: {$mail->ErrorInfo}";
+
+      $mensagem->status['codigo_status'] = 2;
+      $mensagem->status['descricao_status'] = '🙁 Não foi possível enviar este e-mail, por favor tente novamente Detalhes do erro: ' . $mail->ErrorInfo;
+
     }
+
+    ?>
+
+  <html>
+      <head>
+      <meta charset="utf-8" />
+        <title>App Mail Send</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+
+          <div class="container">
+              <div class="py-3 text-center">
+                  <img class="d-block mx-auto mb-2" src="images/logo.png" alt=""  height="80">
+                  <h2>Send Mail</h2>
+                  <p class="lead">Seu app de envio de e-mails particular!</p>
+              </div>
+          </div>
+
+          <div class="row">
+              <div class="col-md-12">
+                  <? if($mensagem->status['codigo_status'] == 1) { ?>
+                      <div class="container">
+                          <h1 class="display-4 text-sucess">Sucesso</h1>
+                          <p><?= $mensagem->status['descricao_status'] ?></p>
+                          <a href="index.php" class="btn btn-sucess btn-lg mt-5 text-white">Voltar</a>
+                      </div>
+                <? } ?>
+
+                  <? if($mensagem->status['codigo_status'] == 2) { ?>
+                    <div class="container">
+                          <h1 class="display-4 text-danger">Ops!</h1>
+                          <p><?= $mensagem->status['descricao_status'] ?></p>
+                          <a href="index.php" class="btn btn-sucess btn-lg mt-5 text-white">Voltar</a>
+                      </div>
+                <? } ?>
+              </div>
+          </div>
+        
+      </body>
+  </html>
